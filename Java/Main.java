054c9b1cc930a8ddd3f43d0b80e1be4b316cd668
Armando Almarales <armandoalmarales@eycon.com>
2022-06-20 18:08:02 -0300

    Initial Commit

---
+++ Java/Main.java
@@ -0,0 +1,20 @@
+public class Main {
+    public static void main(String[] args){
+        Message_1("HelloWorld Bitch");
+        System.out.println(Message_2());
+
+        
+
+    }
+
+    public static void Message_1(String Msg){
+        System.out.println(Msg);
+    }
+
+    public static String Message_2() {
+        return "\nI Love You";
+    }
+
+}
+
+
