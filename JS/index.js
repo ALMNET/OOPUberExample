60d4914369f20c669deb2e0672bba276e5d635a0
Armando Almarales <armandoalmarales@eycon.com>
2022-06-21 23:40:59 -0300

    Js Changes: Car and Account Custom Constructors and console.log functions added.

---
+++ JS/index.js
@@ -0,0 +1,6 @@
+
+var Auto1 = new Car("XOG162", new Account("Armando Almarales", "11296891"));
+var Auto2 = new Car("AOR405", new Account("Armando Jr", "95830022"));
+
+Auto1.printDataCar();
+Auto2.printDataCar();
